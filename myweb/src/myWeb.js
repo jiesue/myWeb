22b912710698ec45531f9aff96e1e2017a87b8ed
yongjie <934164066@qq.com>
2020-01-14 17:00:21 +0800

    删除修改文件

---
+++ myweb/src/myWeb.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '@/src/assets/css/reset.css'
+function MyWeb() {
+    return (
+        <div className="wrap" style={{
+            width: '100%',
+            height: '100%',
+            overflow: 'auto',
+            border:'1px solid red'
+        }}>
+             <h1>jie</h1>
+        </div>
+    )
+}
+
+export default MyWeb;
