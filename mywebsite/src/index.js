c731d96258036cab2500437dc2d6136475ffdf56
yongjie <934164066@qq.com>
2020-01-15 09:48:27 +0800

    new

---
+++ mywebsite/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './page/home'
+import './assets/css/reset.css';
+ReactDOM.render( < Home / > , document.getElementById('root'));
---
+++ mywebsite/src/page/home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import '../assets/css/reset.css'
+function MyWeb() {
+    return (
+        <div className="wrap" style={{
+            width: '100%',
+            height: '100%',
+            overflow: 'auto',
+            border:'1px solid red'
+        }}>
+             <h1>jie1</h1>∏
+        </div>
+    )
+}
+
+export default MyWeb;
