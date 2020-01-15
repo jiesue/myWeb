de8982678428585f72e2d8ba6bac4f47d9f881af
yongjie <934164066@qq.com>
2020-01-15 09:20:29 +0800

    Ï

--- myweb/src/index.js
+++ myweb/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './page/home'
-// import './assets/css/reset.css';
-ReactDOM.render(<Home />, document.getElementById('root'));
-
+import './assets/css/reset.css';
+ReactDOM.render( < Home / > , document.getElementById('root'));
