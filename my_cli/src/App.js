bd629d376d6fb1578bd8142e32ebe04ce1a87514
jie <934164066@qq.com>
2020-04-04 21:13:46 +0800

    my_cli

---
+++ my_cli/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>这个是自己搭建的脚手架 参考步骤 ：<a href='https://www.jianshu.com/p/68e849768d8e'>如何从零开始创建React项目（三种方式）</a> </h1>
+                <h2><a  href="https://segmentfault.com/a/1190000019711348">https://segmentfault.com/a/1190000019711348</a></h2>
+                <h3>待后续学习webpack完善。。。。。</h3>
+
+            </div>
+        )
+    }
+}
+
+export default App
