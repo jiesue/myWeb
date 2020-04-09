0036edc594f228d8a3475e443a79b5856287e02f
zyj <934164066@qq.com>
2020-04-09 15:48:30 +0800

    暂存

--- my_cli/src/main.js
+++ my_cli/src/main.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App.js'
+import App from './App'
 
-ReactDOM.render(<App/>, document.getElementById('app'))
+// console.log(d111);
+ReactDOM.render(<App />, document.getElementById('app'))
